1d6ddfaacc06162d6cfc3beb08bb6696cfba62f1
Airyque <rubyrobinson@Rubys-MacBook-Air.local>
2017-03-09 16:23:22 -0800

    Have server post to database

--- server/server.js
+++ server/server.js
@@ -1,34 +1,13 @@
 const express = require('express');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const webpackConfig = require('../webpack.config.js');
-// const webpackParser = require('webpack-body-parser');
 const User = require('./Users/Users.js');
 const app = express();
 
-const compiler = webpack(webpackConfig);
-
-app.use(express.static(__dirname + '/../www'));
-
-app.use(webpackDevMiddleware(compiler, {
-  hot: true,
-  filename: 'bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
-}));
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-// app.use(webpackParser.json());
-// app.use(webpackParser.urlencoded({extended: false}));
 
 app.post('/test', function(req, res) {
-  console.log('REQ BODY: ', req.body);
   var testUser = new User({username: req.body.username});
   testUser.save(function(err) {
     if (err) {
@@ -38,7 +17,9 @@ app.post('/test', function(req, res) {
       res.send('WE DID IT YYYAAAAAYYYYY!!!!');
     }
   });
-})
+});
+
+app.use(express.static(__dirname + '/../www'));
 
 mongoose.connect('mongodb://localhost/codr', function(err) {
   if (err) {
@@ -47,7 +28,6 @@ mongoose.connect('mongodb://localhost/codr', function(err) {
     console.log('CONNECTED to the database!!');
   }
 });
-
 
 var port = process.env.port || 3000;
 
