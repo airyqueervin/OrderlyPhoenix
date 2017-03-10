19334cf4e17ce4edc9f58c9d4a429ad47dbe60d6
Bluzkry <zou.alexander@gmail.com>
2017-03-09 17:08:10 -0800

    Create instruction template.

---
+++ src/instruction.js
@@ -0,0 +1,21 @@
+import React from 'react';
+require('./../www/main.css');
+
+class Left extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p> Instructions </p>
+        <p> Fill in the loop to help Sally give her friends cookies. </p>
+      </div>
+    );
+  }
+}
+
+export default Left;
