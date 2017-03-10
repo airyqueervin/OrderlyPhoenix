b87da48d364eebc3c2028f45b3e614ef1f7edc9d
Erik Thompson <erik.james.thompson@gmail.com>
2017-03-09 17:07:55 -0800

    Have a Left jsx template

---
+++ src/Learn.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+require('./../www/main.css');
+
+class Learn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+        <p> Learn </p>
+      </div>
+    );
+  }
+}
+
+export default Learn;
