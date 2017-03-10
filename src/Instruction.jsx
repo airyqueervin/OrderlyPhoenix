462b82b0728539a5a91f4f04312b2f8b79747428
nowreen <chowdhry.n@gmail.com>
2017-03-09 17:07:29 -0800

    Create instruction text component

---
+++ src/Instruction.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+require('./../www/main.css');
+
+class Instruction extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p> Instructions </p>
+      </div>
+    );
+  }
+}
+
+export default Instruction;
