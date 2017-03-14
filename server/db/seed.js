cb65f7cd8d3606dbf007d02fa7ed760d717d9947
Erik Thompson <erik.james.thompson@gmail.com>
2017-03-14 14:16:17 -0700

    have an updated game schema and updated seed data

--- server/db/seed.js
+++ server/db/seed.js
@@ -3,7 +3,7 @@ const db = require('./index.js');
 
 Game.remove({}).then(function() {
   Game.create({
-    level: 1, chapter: 1, firstImage: 'firstImage', secondImage: 'secondImage', challengeText: '`<input placeholder="fill in"> apples;`', instructionText: 'Fill in the text below so that the box is labeled', learnText: 'To start coding, we need a way to save the values from our coding.', points: 25, solution: ['var'] 
+    level: 1, chapter: 1, firstImage: 'firstImage', secondImage: 'secondImage', challengeText: '`<input placeholder="fill in"> apples;`', instructionText: 'Fill in the text below so that the box is labeled', learnText: 'To start coding, we need a way to save the values from our coding.', points: 25, solution: ['var'], lastLevel: false 
   }).then(function() {
     console.log('Successfully seeded into game table');
   }).catch(function(err) {
