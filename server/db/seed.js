fc7f3a3c231ae5889610c53452a172b33c66385b
nowreen <chowdhry.n@gmail.com>
2017-03-15 16:19:35 -0700

    Testing for input field change

--- server/db/seed.js
+++ server/db/seed.js
@@ -4,7 +4,7 @@ const User = require('./Users/Users.js');
 
 Game.remove({}).then(function() {
   Game.create({
-    level: 1, chapter: 1, firstImage: 'https://s3-us-west-1.amazonaws.com/codr/L1I1.png', secondImage: 'https://s3-us-west-1.amazonaws.com/codr/L1I2.png', challengeText: '`<input placeholder="fill in"> apples;`', instructionText: 'Fill in the text below so that the box is labeled', learnText: 'To start coding, we need a way to save the values from our coding.', points: 25, solution: ['var'], lastLevel: false 
+    level: 1, chapter: 1, firstImage: 'https://s3-us-west-1.amazonaws.com/codr/L1I1.png', secondImage: 'https://s3-us-west-1.amazonaws.com/codr/L1I2.png', challengeText: 'abcd apples', instructionText: 'Fill in the text below so that the box is labeled', learnText: 'To start coding, we need a way to save the values from our coding.', points: 25, solution: ['var'], lastLevel: false 
   }).then(function() {
     console.log('Successfully seeded level data into database');
   }).catch(function(err) {
