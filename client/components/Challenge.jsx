246fb85a87e2fc7728ca9c91a4d95ffac76bd89f
Airyque <rubyrobinson@Rubys-MacBook-Air.local>
2017-03-15 15:45:06 -0700

    Add extra methods to challenge and update state

--- client/components/Challenge.jsx
+++ client/components/Challenge.jsx
@@ -4,13 +4,37 @@ require('./../../public/main.css');
 class Challenge extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {splitText: "bullshit"};
+    this.updateChallenge = this.updateChallenge.bind(this);
+    // this.splitChallengeText = this.splitChallengeText.bind(this);
+    // this.splitChallengeText();
   }
+
+  updateChallenge(e) {
+    console.log('I CHANGED', e.target.value);
+    this.setState({challengeTex: e.target.value});
+  }
+
+  // splitChallengeText(){ 
+  //  this.setState({splitText: this.props.chapter ? this.props.chapter[0].challengeText.split(' ') : null});
+  // }
+
+  componentDidMount() {
+    this.setState({
+      splitText: this.props.chapter[0].challengeText.split(' ')
+    });
+  }
+
+  // joinChallengeText() {
+
+  // }
+  
 
   render() {
     return (
       <div>
         <strong><p> Challenge Text </p></strong>
+        <input onChange={this.updateChallenge} type="text" placeholder="Some text to fill in"/>
         <div dangerouslySetInnerHTML={{__html: this.props.chapter ? this.props.chapter[0].challengeText : null}} />
       </div>
     );
