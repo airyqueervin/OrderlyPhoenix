c21a45a0be0be486845c656a5ee8a1b5cca055dc
nowreen <chowdhry.n@gmail.com>
2017-03-16 15:01:03 -0700

    Update seed to pull from data.json

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,12 +1,15 @@
 const Game = require('./Game/Game.js');
 const db = require('./index.js');
+const data = require('./../../data.json');
 
 Game.remove({}).then(function() {
-  Game.create({
-    level: 1, chapter: 1, firstImage: 'https://s3-us-west-1.amazonaws.com/codr/lvl_1_img_1.png', secondImage: 'https://s3-us-west-1.amazonaws.com/codr/lvl_1_img_2.png', challengeText: ['input1', 'apples', 'input2', 'banana'], instructionText: 'Fill in the text below so that the box is labeled.', learnText: 'To start coding, we need a way to save the values from our coding.', points: 25, solution: [{input1: 'var', input2: 'var'}]
-  }).then(function() {
-    console.log('Successfully seeded level data into database');
-  }).catch(function(err) {
-    console.log('ERROR seeding level into db: ', err);
+  data.forEach(function(level) {
+    Game.create(level)
+    .then(function() {
+      console.log('Successfully seeded level ' + level.level + ' data into database');
+    })
+    .catch(function(err) {
+      console.log('ERROR seeding level into db: ', err);
+    });
   });
 });
