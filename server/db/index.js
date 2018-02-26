21f5cbd636a56399052f38a878129460f7ee0164
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-02-26 12:54:34 -0800

    Modifying for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost/codr', function(err) {
+// const db = mongoose.connect('mongodb://localhost/codr', function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('CONNECTED to the database!!');
+//   }
+// });
+const db = mongoose.connect('mongodb://heroku_swfvg5p2:n08t0csqchs9t8elhm0tufj3s5@ds249418.mlab.com:49418/heroku_swfvg5p2', function(err) {
   if (err) {
     console.log(err);
   } else {
